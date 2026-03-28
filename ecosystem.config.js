868993353fc685ef5db8d2b2e19d6d037b01e85d
A.M. Supun Madhuranga <157113777+smadhuranga@users.noreply.github.com>
2026-03-28 18:08:36 +0530

    Update ecosystem.config.js to output separate PM2 error and out logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,23 @@
 module.exports = {
   apps : [{
-    name   : "config-server",
-    script : "java -jar ./config-server/target/config-server-0.0.1-SNAPSHOT.jar"
+    name       : "config-server",
+    script     : "java -jar ./config-server/target/config-server-0.0.1-SNAPSHOT.jar",
+    out_file   : "./logs/config-server-out.log",
+    error_file : "./logs/config-server-error.log",
+    merge_logs : true
   },
   {
-    name   : "service-registry",
-    script : "java -jar ./service-registry/target/eureka-server-0.0.1-SNAPSHOT.jar"
+    name       : "service-registry",
+    script     : "java -jar ./service-registry/target/eureka-server-0.0.1-SNAPSHOT.jar",
+    out_file   : "./logs/service-registry-out.log",
+    error_file : "./logs/service-registry-error.log",
+    merge_logs : true
   },
   {
-    name   : "api-gateway",
-    script : "java -jar ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar"
+    name       : "api-gateway",
+    script     : "java -jar ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
+    out_file   : "./logs/api-gateway-out.log",
+    error_file : "./logs/api-gateway-error.log",
+    merge_logs : true
   }]
 }
