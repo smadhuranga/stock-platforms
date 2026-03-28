b0e978976b52680bbad5ada723c19d3700e657d6
A.M. Supun Madhuranga <157113777+smadhuranga@users.noreply.github.com>
2026-03-28 16:26:24 +0530

    Add pm2 ecosystem config file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name   : "config-server",
+    script : "java -jar ./config-server/target/config-server-0.0.1-SNAPSHOT.jar
+"
+  },
+  {
+    name   : "service-registry",
+    script : "java -jar ./service-registry/target/eureka-server-0.0.1-SNAPSHOT.jar
+"
+  },
+  {
+    name   : "api-gateway",
+    script : "java -jar ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar"
+  }]
+}
