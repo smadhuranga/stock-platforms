6d3a03d8370df0f53e56c610bfbb852364f7234c
A.M. Supun Madhuranga <157113777+smadhuranga@users.noreply.github.com>
2026-03-28 18:09:51 +0530

    Re-add PM2 logging output configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,23 @@
 module.exports = {
   apps : [{
-    name       : "config-server",
-    script     : "java -jar ./config-server/target/config-server-0.0.1-SNAPSHOT.jar",
-    out_file   : "./logs/config-server-out.log",
-    error_file : "./logs/config-server-error.log",
-    merge_logs : true
+    name   : "config-server",
+    script : "java -jar ./config-server/target/config-server-0.0.1-SNAPSHOT.jar",
+    out_file: "./logs/config-server.log",
+    error_file: "./logs/config-server-error.log",
+    merge_logs: true
   },
   {
-    name       : "service-registry",
-    script     : "java -jar ./service-registry/target/eureka-server-0.0.1-SNAPSHOT.jar",
-    out_file   : "./logs/service-registry-out.log",
-    error_file : "./logs/service-registry-error.log",
-    merge_logs : true
+    name   : "service-registry",
+    script : "java -jar ./service-registry/target/eureka-server-0.0.1-SNAPSHOT.jar",
+    out_file: "./logs/service-registry.log",
+    error_file: "./logs/service-registry-error.log",
+    merge_logs: true
   },
   {
-    name       : "api-gateway",
-    script     : "java -jar ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
-    out_file   : "./logs/api-gateway-out.log",
-    error_file : "./logs/api-gateway-error.log",
-    merge_logs : true
+    name   : "api-gateway",
+    script : "java -jar ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
+    out_file: "./logs/api-gateway.log",
+    error_file: "./logs/api-gateway-error.log",
+    merge_logs: true
   }]
 }
